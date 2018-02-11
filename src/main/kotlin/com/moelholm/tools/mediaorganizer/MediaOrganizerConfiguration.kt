948b5b0635224ad9684237a951630dcf40c3dd52
moelholm <moelholm@gmail.com>
2018-02-11 13:11:00 +0100

    converted configuration to Kotlin

---
+++ src/main/kotlin/com/moelholm/tools/mediaorganizer/MediaOrganizerConfiguration.kt
@@ -0,0 +1,37 @@
+package com.moelholm.tools.mediaorganizer
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+import java.util.Arrays
+import java.util.Locale
+
+@Component
+class MediaOrganizerConfiguration {
+
+    @Value("\${daemon.scheduleAsCronExpression}")
+    val scheduleAsCronExpression: String? = null
+
+    @Value("\${mediafiles.datepattern}")
+    val mediaFilesDatePattern: String? = null
+
+    @Value("\${mediafiles.mediaFileExtensionsToMatch}")
+    private val mediaFileExtensionsToMatch: Array<String>? = null
+
+    @Value("\${destination.amountOfMediaFilesIndicatingAnEvent}")
+    val amountOfMediaFilesIndicatingAnEvent: Int = 0
+
+    @Value("\${destination.localeForGeneratingDestinationFolderNames}")
+    val locale: Locale? = null
+
+    @Value("\${destination.suffixForDestinationFolderOfUnknownEventMediaFiles}")
+    val suffixForDestinationFolderOfUnknownEventMediaFiles: String? = null
+
+    @Value("\${destination.suffixForDestinationFolderOfMiscMediaFiles}")
+    val suffixForDestinationFolderOfMiscMediaFiles: String? = null
+
+    fun getMediaFileExtensionsToMatch(): List<String> {
+        return Arrays.asList(*mediaFileExtensionsToMatch!!)
+    }
+
+}
